8404e7deaa6402181969791aa818f33744d51754
Victor Chiang <vgchiang@gmail.com>
2020-11-20 15:56:31 -0600

    complete challenge

--- 04-Front-End/04-JavaScript-basics/04-Unordered-list/lib/list_generator.js
+++ 04-Front-End/04-JavaScript-basics/04-Unordered-list/lib/list_generator.js
@@ -1,9 +1,18 @@
 const listItem = (content) => {
   // TODO: return the proper <li> HTML tag with its content (as a string)
+  return `<li class="list-group-item">${content}</li>`;
 };
 
 const unorderedList = (items) => {
   // TODO: return the proper <ul> markup (as a string)
+  const beginning = '<ul class="list-group">';
+  const end = '</ul>';
+
+  const newItems = items.map((item) => {
+    return `<li class="list-group-item">${item}</li>`;
+  });
+
+  return beginning + newItems.toString().replace(',', '') + end;
 };
 
 module.exports = { listItem, unorderedList }; // Do not remove.
