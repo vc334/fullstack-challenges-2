4736a3c5b3f49d1ed1f6c583ae760b4d0d1fa12d
Victor Chiang <vgchiang@gmail.com>
2020-11-20 15:02:20 -0600

    completed challenge

--- 04-Front-End/04-JavaScript-basics/03-Valid-email/lib/valid_email.js
+++ 04-Front-End/04-JavaScript-basics/03-Valid-email/lib/valid_email.js
@@ -1,5 +1,6 @@
 const valid = (email) => {
   // TODO: return true if the `email` string has the right pattern!
+  return /.+@.+[.][a-z]{2,3}/.test(email);
 };
 
 module.exports = valid; // Do not remove.
