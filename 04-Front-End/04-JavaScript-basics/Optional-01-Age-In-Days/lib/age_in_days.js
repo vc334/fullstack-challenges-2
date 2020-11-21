9f44fd3a5680990ecdf89072f3b4534de0438620
Victor Chiang <vgchiang@gmail.com>
2020-11-21 14:06:40 -0600

    completed challenge

--- 04-Front-End/04-JavaScript-basics/Optional-01-Age-In-Days/lib/age_in_days.js
+++ 04-Front-End/04-JavaScript-basics/Optional-01-Age-In-Days/lib/age_in_days.js
@@ -1,5 +1,8 @@
 const ageInDays = (day, month, year) => {
   // TODO: Implement the method and return an integer
+  const birthday = new Date(year, month - 1, day);
+  const age = new Date() - birthday;
+  return Math.round((age / 1000) / 86400);
 };
 
 module.exports = ageInDays; // Do not remove. We need this for the spec to know about your method.
