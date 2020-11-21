06a2c161ea5b502e01ebebd3d39c452804439ee8
Victor Chiang <vgchiang@gmail.com>
2020-11-21 14:43:50 -0600

    completed challenge

--- 04-Front-End/04-JavaScript-basics/Optional-02-Split-The-Bill/lib/split_the_bill.js
+++ 04-Front-End/04-JavaScript-basics/Optional-02-Split-The-Bill/lib/split_the_bill.js
@@ -1,5 +1,25 @@
 const splitTheBill = (group) => {
   // TODO: implement the function and return an Object
+  let total = 0;
+  let individuals = 0;
+
+  Object.keys(group).forEach((key) => {
+    const value = group[key];
+    total += value;
+    individuals += 1;
+  });
+
+  const perPerson = total / individuals;
+
+  const group2 = group;
+
+  Object.keys(group2).forEach((key) => {
+    const value = group2[key];
+    const newValue = value - perPerson;
+    group2[key] = newValue;
+  });
+
+  return group2;
 };
 
 module.exports = splitTheBill; // Do not remove this line.
