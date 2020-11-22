5fcea064cdf25a6ffd2d7633c6a79a7c378c7fb0
Victor Chiang <vgchiang@gmail.com>
2020-11-22 16:49:55 -0600

    completed challenge

--- 04-Front-End/05-DOM-and-Events/03-My-First-Event-Listener/lib/listener.js
+++ 04-Front-End/05-DOM-and-Events/03-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,12 @@
 // TODO: React to a click on the button!
+const button = document.querySelector("#clickme");
+const audio = new Audio('sound.mp3');
+
+button.addEventListener("click", (e) => {
+  console.log(e);
+  console.log(e.target);
+  console.log(e.currentTarget);
+  e.target.classList.add("disabled");
+  e.target.innerText = "Bingo!";
+  audio.play();
+});
