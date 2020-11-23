bd00be2a43c62f0e707d8e041b8cd7a3b8ecb9ad
Victor Chiang <vgchiang@gmail.com>
2020-11-23 12:31:45 -0600

    completed challenge

--- 04-Front-End/05-DOM-and-Events/04-Wagon-Race/lib/wagon_race.js
+++ 04-Front-End/05-DOM-and-Events/04-Wagon-Race/lib/wagon_race.js
@@ -1,1 +1,29 @@
 // TODO: write your code here
+// moving the board
+
+const move = (player) => {
+  const wagon = document.querySelector(`#player${player}-race td.active`)
+  console.log(wagon);
+  if (wagon.nextElementSibling) {
+    wagon.classList.remove("active");
+    wagon.nextElementSibling.classList.add("active")
+  } else {
+    alert(`Player ${player} wins! Play again?`)
+    window.location.reload();
+  }
+};
+
+
+//moving the key
+
+const moveWagons = (e) => {
+  if (e.key === 'q') {
+    move(1);
+    console.log(e);
+  } else if (e.key === 'p') {
+    move(2);
+    console.log(e);
+  }
+};
+
+document.addEventListener("keyup", moveWagons);
