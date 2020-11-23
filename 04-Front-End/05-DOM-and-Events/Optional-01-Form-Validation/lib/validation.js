f756f21c51874e434221fa7603b80ece92f57617
Victor Chiang <vgchiang@gmail.com>
2020-11-23 17:52:49 -0600

    completed challenge

--- 04-Front-End/05-DOM-and-Events/Optional-01-Form-Validation/lib/validation.js
+++ 04-Front-End/05-DOM-and-Events/Optional-01-Form-Validation/lib/validation.js
@@ -1,1 +1,23 @@
 // TODO: Validate this form
+const allInputs = document.querySelectorAll(".form-control");
+const allInputsArr = Array.from(allInputs);
+const checkbox = document.getElementById("tos");
+const btn = document.querySelector("button");
+
+const filled = input => input.value;
+const allFilled = () => allInputsArr.every(filled);
+
+
+
+const enableButton = () => {
+  const checkTheBox = checkbox.checked;
+  if (allFilled() && checkTheBox) {
+    btn.disabled = false;
+  }
+}
+
+allInputs.forEach((input) => {
+  input.addEventListener("blur", enableButton);
+});
+
+checkbox.addEventListener("click", enableButton);
