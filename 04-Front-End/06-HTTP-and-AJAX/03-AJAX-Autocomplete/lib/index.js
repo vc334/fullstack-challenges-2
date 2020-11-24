7ed35222fc0bedd0a33b971515422d81c7b4efa8
Victor Chiang <vgchiang@gmail.com>
2020-11-24 14:31:17 -0600

    completed challenge

--- 04-Front-End/06-HTTP-and-AJAX/03-AJAX-Autocomplete/lib/index.js
+++ 04-Front-End/06-HTTP-and-AJAX/03-AJAX-Autocomplete/lib/index.js
@@ -1,1 +1,26 @@
 // TODO: Autocomplete the input with AJAX calls.
+const searchInput = document.getElementById("search");
+console.log(searchInput);
+const list = document.getElementById("results");
+
+const getTheWord = (e) => {
+  const newWord = e.currentTarget.value;
+  list.innerHTML = '';
+  updateList(newWord);
+}
+
+
+const updateList = (searchWord) => fetch(`https://wagon-dictionary.herokuapp.com/autocomplete/${searchWord}`)
+  .then(response => response.json())
+  .then((data) => {
+    data.words.forEach((word) => {
+      const newListItem = `
+        <li>
+          ${word}
+        </li>
+      `;
+      list.insertAdjacentHTML('beforeend', newListItem);
+    });
+  });
+
+searchInput.addEventListener('keyup', getTheWord);
