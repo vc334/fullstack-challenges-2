5e14caa94a46f38f4f36043b71b9ff9d080e7a4f
Victor Chiang <vgchiang@gmail.com>
2020-11-24 13:30:53 -0600

    completed challenge

--- 04-Front-End/06-HTTP-and-AJAX/02-My-Favourite-Sports/lib/index.js
+++ 04-Front-End/06-HTTP-and-AJAX/02-My-Favourite-Sports/lib/index.js
@@ -1,1 +1,13 @@
 // TODO: write your code here!
+const boxes = document.querySelectorAll(".clickable");
+console.log(boxes);
+
+const toggle = (e) => {
+  e.currentTarget.classList.toggle("active");
+};
+
+const listenerEvent = (box) => {
+  box.addEventListener('click', toggle);
+};
+
+boxes.forEach(listenerEvent);
