a389f788b574aff6099edd50a68294e92ff68d9b
Victor Chiang <vgchiang@gmail.com>
2020-11-25 17:17:38 -0600

    refactored challenge code

--- 04-Front-End/06-HTTP-and-AJAX/Optional-01-Booking-form/lib/index.js
+++ 04-Front-End/06-HTTP-and-AJAX/Optional-01-Booking-form/lib/index.js
@@ -1,12 +1,13 @@
 // TODO: write your code here!
 const minus = document.querySelector(".actions .incrementer:first-child");
-console.log(minus);
+// console.log(minus);
 const incrementer = document.querySelector(".actions .incrementer:last-child");
-console.log(minus);
+// console.log(minus);
 counter = document.getElementById("counter");
 const valueForm = document.getElementById("price-input");
 const fixedValue = valueForm.value;
 const submitButton = document.getElementById("submit");
+const currentCount = () => Number.parseInt(counter.dataset.count, 10);
 
 const updateSubmitButtonValue = (count) => {
   submitButton.value = `Pay ${count.toString() / 100}€`;
@@ -17,37 +18,33 @@ const valueFormDisabled = () => {
 }
 
 const increaseCounter = () => {
-  const currentCount = Number.parseInt(counter.dataset.count, 10);
-  const newCount = currentCount + 1
+  const newCount = currentCount() + 1
   counter.innerText = newCount;
   counter.dataset.count = newCount.toString();
   valueForm.value = fixedValue * newCount;
   updateSubmitButtonValue(valueForm.value);
   if (newCount > 1) {
-    valueForm.disabled = false;
+    valueFormDisabled();
     submitButton.disabled = false;
   }
 };
 
 const decreaseCounter = () => {
-  const currentCount = Number.parseInt(counter.dataset.count, 10);
-
-  if (currentCount === 2) {
-    const newCount = currentCount - 1
+  if (currentCount() === 2) {
+    const newCount = currentCount() - 1
   counter.innerText = newCount;
   counter.dataset.count = newCount.toString();
   valueForm.value = fixedValue * newCount;
   valueForm.disabled = true;
-  updateSubmitButtonValue(valueForm.value);
   submitButton.disabled = true;
-  } else if (currentCount > 1) {
-    const newCount = currentCount - 1
+  } else if (currentCount() > 1) {
+    const newCount = currentCount() - 1
   counter.innerText = newCount;
   counter.dataset.count = newCount.toString();
   valueForm.value = fixedValue * newCount;
-  valueForm.disabled = false;
-  updateSubmitButtonValue(valueForm.value);
+  valueFormDisabled();
   }
+  updateSubmitButtonValue(valueForm.value);
 };
 
 minus.addEventListener("click", decreaseCounter);
